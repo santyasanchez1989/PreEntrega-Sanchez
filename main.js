aaf3aae5e9ee6e06c528c26741ec5dd6fd0e3990
Santi Sanchez <santiagustinsanchez@gmail.com>
2023-08-24 19:59:58 -0300

    modificando completo el trabajo para que cuadre con mi pagina de desarrollo.

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
-const lado1Input = document.getElementById('lado1');
-const lado2Input = document.getElementById('lado2');
-const calcularSuperficieButton = document.getElementById('calcularSuperficie');
-const resultadoOutput = document.getElementById('resultado');
-const historialLista = document.getElementById('historial');
+const botonEnviar = document.getElementById('enviar');
+const inputNombres = document.getElementById('nombres');
+const inputFechaHora = document.getElementById('fechaHora');
+const contenedorCitas = document.getElementById('citas');
 
-const superficiesArray = [];
+let citas = [];
 
+botonEnviar.addEventListener('click', () => {
+  const nombres = inputNombres.value;
+  const fechaHora = inputFechaHora.value;
 
-function calcularSuperficieRectangulo(lado1, lado2) {
-  const superficie = lado1 * lado2;
-  return superficie;
-}
-
-calcularSuperficieButton.addEventListener('click', function () {
-  const lado1 = parseFloat(lado1Input.value);
-  const lado2 = parseFloat(lado2Input.value);
-
-  if (isNaN(lado1) || isNaN(lado2)) {
-      resultadoOutput.textContent = "Por favor, ingresa valores numéricos válidos para ambos lados.";
-  } else {
-      const superficieRectangulo = calcularSuperficieRectangulo(lado1, lado2);
-      resultadoOutput.textContent = `La superficie del rectángulo es: ${superficieRectangulo.toFixed(2)} mts2`;
-      
-      superficiesArray.push(superficieRectangulo);
-      actualizarHistorial();
+  if (nombres && fechaHora) {
+    const cita = { nombres, fechaHora };
+    citas.push(cita);
+    actualizarHorario();
+    guardarCitasEnLocalStorage();
+    inputNombres.value = '';
+    inputFechaHora.value = '';
   }
 });
 
-function actualizarHistorial() {
-  historialLista.innerHTML = '';
-  for (const superficie of superficiesArray) {
-      const listaItem = document.createElement('li');
-      listaItem.textContent = `Superficie en mts2: ${superficie.toFixed(2)}`;
-      historialLista.appendChild(listaItem);
+function actualizarHorario() {
+  contenedorCitas.innerHTML = '<h2>Horario de Citas</h2>';
+  if (citas.length === 0) {
+    contenedorCitas.innerHTML += '<p>No hay citas programadas</p>';
+  } else {
+    citas.forEach((cita, index) => {
+      const { nombres, fechaHora } = cita;
+      contenedorCitas.innerHTML += `
+        <div class="cita">
+          <p>${nombres} - ${fechaHora}</p>
+          <button onclick="cancelarCita(${index})">Cancelar</button>
+        </div>`;
+    });
   }
 }
+
+function cancelarCita(index) {
+  citas.splice(index, 1);
+  actualizarHorario();
+  guardarCitasEnLocalStorage();
+}
+
+function guardarCitasEnLocalStorage() {
+  localStorage.setItem('citas', JSON.stringify(citas));
+}
+
+function cargarCitasDesdeLocalStorage() {
+  const citasAlmacenadas = localStorage.getItem('citas');
+  if (citasAlmacenadas) {
+    citas = JSON.parse(citasAlmacenadas);
+  }
+}
+
+cargarCitasDesdeLocalStorage();
+actualizarHorario();
